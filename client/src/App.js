cf4db393e4d085e39aa581c5072e3d520c91012f
momaki9 <mostafa_m9@yahoo.com>
2023-04-30 18:36:53 -0700

    implemented mui as css framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import Home from "./pages/Home";
+import Bar from "./components/Bar";
 
 function App() {
   return (
     <div>
+      <Bar />
       <h1>HealtheeByte - Coming soon!</h1>
+      <Home />
     </div>
   );
 }
