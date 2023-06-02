af32fcd63ee1bce49df56bfb0e69de91fc5fee0a
momaki9 <mostafa_m9@yahoo.com>
2023-06-01 18:43:02 -0700

    installed react Quill

--- client/src/pages/DevMode.js
+++ client/src/pages/DevMode.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_RECIPES } from "../utils/queries";
 
-const DevMode = () => {
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
+const DevMode = () => {
+    const [ value, setValue ] = useState('');
     const { loading, data } = useQuery(QUERY_RECIPES);
     const allRecipes = data?.allRecipes || [];
-    console.log(allRecipes);
+    // console.log(allRecipes);
+    console.log(value);
     return (
         <>
         <h2>To Do's</h2>
@@ -19,6 +24,7 @@ const DevMode = () => {
             <li style={{margin: '1%', fontSize: '23px'}}>Figure out how to include images</li>
             <li style={{margin: '1%', fontSize: '23px'}}>Figure out how to embed youtube videos</li>
         </ol>
+        <ReactQuill theme="snow" value={value} onChange={setValue}/>
         </>
     )
 };
