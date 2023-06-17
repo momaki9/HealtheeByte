7468d0874996223c54b2ccdce004d9d6cebd4921
momaki9 <mostafa_m9@yahoo.com>
2023-06-17 13:47:09 -0700

    updated dev notes

--- client/src/pages/DevMode.js
+++ client/src/pages/DevMode.js
@@ -4,20 +4,22 @@ const DevMode = () => {
         <>
             <h2>To Do's</h2>
             <ol style={{ margin: '2%' }}>
-                <li style={{ margin: '1%', fontSize: '23px' }}>Add Recipes to DB</li>
-                <ul>
-                    <li style={{ margin: '1%', fontSize: '23px' }}>Create a page for each recipe that shows all details of the recipe</li>
-                </ul>
+                <li style={{ margin: '1%', fontSize: '23px' }}>Allow users to add recipes to their favorites (Model needs updating).</li>
+                <li style={{ margin: '1%', fontSize: '23px' }}>Allow users to view recipes they have created</li>
                 <li style={{ margin: '1%', fontSize: '23px' }}>Implement Recipe Search Functionality</li>
+                <li style={{ margin: '1%', fontSize: '23px' }}>Deploy app to Heroku</li>
                 <li style={{ margin: '1%', fontSize: '23px' }}>Allow users to delete and update recipe/profile</li>
                 <ul>
                     <li style={{ margin: '1%', fontSize: '23px' }}>Add image link or video link</li>
+                    <li style={{ margin: '1%', fontSize: '23px' }}>Update username? Profile picture? (Model needs updating)</li>
                 </ul>
-                <li style={{ margin: '1%', fontSize: '23px' }}>Create a profile page</li>
+                <li style={{ margin: '1%', fontSize: '23px' }}>Work on updating the profile page</li>
                 <li style={{ margin: '1%', fontSize: '23px' }}>Allow logged in users to save recipes</li>
                 <li style={{ margin: '1%', fontSize: '23px' }}>Implement Logout Functionality</li>
-                <li style={{ margin: '1%', fontSize: '23px' }}>Figure out how to include images</li>
-                <li style={{ margin: '1%', fontSize: '23px' }}>Work on creating a Recipe Outline page</li>
+                <ul>
+                    <li style={{ margin: '1%', fontSize: '23px' }}>Need logout/sign in buttons to 'know' when a user is logged in</li>
+                </ul>
+                <li style={{ margin: '1%', fontSize: '23px' }}>Figure out how to include images from user upload</li>
                 <li style={{ margin: '1%', fontSize: '23px' }}>Figure out how to embed youtube videos</li>
                 <h3>Future Development:</h3>
                 <li style={{ margin: '1%', fontSize: '23px' }}>Allow users to include images into their recipes as well as resize/position them as needed</li>
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -21,7 +21,7 @@ class AuthService {
     }
     login(idToken) {
         localStorage.setItem('id_token', idToken);
-        window.location.assign('/');
+        window.location.assign('/addrecipe');
     }
     logout() {
         localStorage.removeItem('id_token');
